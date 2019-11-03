9db109ac695d79ad04733fbcc59909030fe4a50f
Anders Peterson <anderspeterson11@gmail.com>
2019-11-03 07:41:52 -0500

    anonymized

--- DSO510ProjectAmazonReviews.py
+++ DSO510ProjectAmazonReviews.py
@@ -2,17 +2,17 @@ import pandas as pd
 import statistics
 import glob
 
+#import certain columns of a .tsv file and set the datatypes of the columns
 def importClean(datasetFile):
     df = pd.read_csv(datasetFile, sep='\t', usecols = [6,7,8,9,11,14])
-    #df = df.sample(6000)
     df['review_date'] = pd.to_datetime(df['review_date'], errors='coerce')
     df['star_rating'] = pd.to_numeric(df['star_rating'], errors='coerce')
     df['helpful_votes'] = pd.to_numeric(df['helpful_votes'], errors='coerce')
     df['total_votes'] = pd.to_numeric(df['total_votes'], errors='coerce')
-    #print(df)
     
     return df
 
+#get the average star rating for a selection of months as well as the avg number of reviews for each month
 def avgRating(dataset, months):
     selectedMonths = []
     for month in months:
@@ -25,13 +25,13 @@ def avgRating(dataset, months):
     return statistics.mean(selectedMonths)
 
 
-
+#create an empty dataframe to be added to later
 reviews = pd.DataFrame(columns = ['star_rating','review_date'])
 
 #modify this to set the path to where the datasets are
-path = '/home/ec2-user/reviews/USreviews/'
+path = ''
 
-#use this if you want to look at only one .csv file
+#use this if you want to look at only one .csv file (Gift_Card is the smallest dataset)
 #allFiles = glob.glob(path + 'amazon_reviews_us_Gift_Card_v1_00.tsv')
 
 allFiles = glob.glob(path + 'amazon_reviews_us_*.tsv')
@@ -44,4 +44,4 @@ print('--------------------------')
 print('avg rating:', avgRating(reviews,[2,3,4,5,6,7,8,9,10]))
 
 #set this to the path where you want the subset file to go
-reviews.to_csv('/home/ec2-user/COMPLETEREVIEWS.csv')
+reviews.to_csv('')
